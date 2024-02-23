7fdcf32d7e39aac4b95cdc118cc1bfddbe9b235c
eskainah <eskainah@gmail.com>
2024-02-23 23:44:04 +0200

    create a function that converts input to integers

--- script.js
+++ script.js
@@ -1,86 +1,75 @@
-    const num1 = 12;
-    const num2 = 7;
-    const operators = {
-        add: "+",
-        subtract: "-",
-        multiply: "*",
-        divide: "/"
-    };
-let sign = ""; 
+const operators = {
+    add: "+",
+    subtract: "-",
+    multiply: "*",
+    divide: "/"
+};
+let operand_One = "";
+let operand_Two = ""
+let sign = "";
 
-const operator =  document.querySelectorAll('.sign').forEach(function(selectedOperator){
-                selectedOperator.addEventListener('click', ()=>{
-                    const selected = event.target.textContent;
-                    //checks if math symbol selected 
-                    //This condition uses the ternary operations
-                    sign = 
-                        operators.add===selected ? "+":
-                        operators.subtract===selected ? "-":
-                        operators.multiply===selected ? "*":
-                        operators.divide===selected ? "/" :
-                        "";
-        })
-    });
- 
-  
-    function add(a, b){
-        return a + b;
-    }
-    /* 
-    console.log(add(num1, num2))
+const operator = document.querySelectorAll('.sign').forEach(function (selectedOperator) {
+    selectedOperator.addEventListener('click', () => {
+        const selected = event.target.textContent;
+        //checks if math symbol selected using the ternary operations
+        sign =
+            operators.add === selected ? "+" :
+            operators.subtract === selected ? "-" :
+            operators.multiply === selected ? "*" :
+            operators.divide === selected ? "/" :
+            "";
+    })
+});
 
-    function subtract(a, b){
-        return a - b;
-    }
-    console.log(subtract(num1, num2))
-
-    function multiply(a, b){
-        return a * b;
-    }
-    console.log(multiply(num1, num2))
-    
-    function divide(a, b){
-        return a / b;
-    }
-    
-    console.log(divide(num1, num2)); */
-
-    function operate(){
-        while(sign ===""){
-          
-        if (sign === "+"){
-                console.log(add(num1, num2));   
-            }
-        console.log(sign)
-    }
-    }
-  
-   /*  var edNume ="1020"
-    console.log(typeof(edNume));
-    no = parseInt(edNume);
-    console.log(typeof(no)) */
-
-    /* A function collect inputs */
-    let value_One="";
-    const operand = document.querySelectorAll('.btn').forEach(function(digit){
-        const display_Inputs = document.querySelector('.userInput') 
-        digit.addEventListener('click', ()=>{
-            value_One += event.target.textContent;  //store the num(s) click
-            display_Inputs.textContent = value_One;
-        })
-    });
-    
+function add(a, b) {
+    return a + b;
+}
 /* 
-    const operator =  document.querySelectorAll('.sign').forEach(function(selectedOperator){
-        selectedOperator.addEventListener('click', ()=>{
-            const selected = event.target.textContent;
-            //checks if math symbol selected 
-            //This condition uses the ternary operations
-            sign = 
-                operators.add===selected ? "+":
-                operators.subtract===selected ? "-":
-                operators.multiply===selected ? "*":
-                operators.divide===selected ? "/" :
-                "";
-})
-}); */
+console.log(add(num1, num2))
+
+function subtract(a, b){
+    return a - b;
+}
+console.log(subtract(num1, num2))
+
+function multiply(a, b){
+    return a * b;
+}
+console.log(multiply(num1, num2))
+ 
+function divide(a, b){
+    return a / b;
+}
+ 
+console.log(divide(num1, num2)); */
+
+function operate() {
+    convertInput()
+        if (sign === "+") {
+            console.log(add(operand_One, 12));
+        }
+        console.log(sign)
+}
+
+/* A function collect inputs */
+let user_Input = "";
+
+const operand = document.querySelectorAll('.btn').forEach(function (digit) {
+    const display_Inputs = document.querySelector('.userInput')
+    digit.addEventListener('click', () => {
+        digit.textContent != "DEL" && digit.textContent != "AC" && digit.textContent != "=" ? user_Input += event.target.textContent: undefined; //store the num(s) click before an operator
+        display_Inputs.textContent = user_Input;// display value on screen 
+    })
+});
+/* sign === "" ? console.log("Hey") : console.log("Keep on"); */
+
+
+function convertInput(){
+    if(user_Input[user_Input.length -1] === "+"){
+        sliceInput = user_Input.slice(0,-1);// remove the last character
+        console.log(sliceInput)
+        operand_One === "" ? operand_One = parseInt(sliceInput): operand_Two = parseInt(user_Input)
+        console.log(typeof(operand_One))
+    }
+}
+
