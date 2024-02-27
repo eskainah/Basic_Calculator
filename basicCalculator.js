5132f20d4364f0923b753e0fbda5f3ae2b51f88d
eskainah <eskainah@gmail.com>
2024-02-27 22:38:23 +0200

    declare globalvariiables

---
+++ basicCalculator.js
@@ -0,0 +1,12 @@
+const operators = {
+    add: "+",
+    subtract: "-",
+    multiply: "*",
+    divide: "/"
+};
+let operand_One = null;
+let operand_Two = null;
+let sign = null;
+let user_Input = "";
+let result = "";
+let display_Output = document.querySelector('.calOutput');
