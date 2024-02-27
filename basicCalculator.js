57876d6fe19659dbd3d2b48afbaf3876cb781798
eskainah <eskainah@gmail.com>
2024-02-27 22:42:06 +0200

    create a function that collect user inputs

--- basicCalculator.js
+++ basicCalculator.js
@@ -33,3 +33,46 @@ const operator = document.querySelectorAll('.sign').forEach(function (selectedOp
       operate()
       });
 });
+
+function getInput(){
+    const display_Inputs = document.querySelector('.userInput'); 
+    const operand = document.querySelectorAll('.btn').forEach(function (digit) {
+      digit.addEventListener('click', () => { 
+          //store the num(s) click before an operator
+        if(digit.textContent != "DEL" && digit.textContent != "AC" && digit.textContent != "="){
+          
+          if (user_Input === "") {
+            if (event.target.textContent === "/" || event.target.textContent === "*" || /* event.target.textContent === "-" ||  */event.target.textContent === "+" || event.target.textContent === "=") {
+              user_Input = ""
+            } 
+            else{
+              user_Input +=event.target.textContent;
+            }
+            //accept decimals as first input
+            if(event.target.textContent === "."){
+              user_Input = 0 + event.target.textContent;
+            }
+          }
+          else{
+            if (result != "") {
+              user_Input = result + event.target.textContent;
+              result=""
+              operand_One=""
+              operand_Two=""
+              sign=""
+            }
+            else{
+              //allow decimals point after an operation 
+              if(event.target.textContent === "."){
+                user_Input += 0 + event.target.textContent;
+              }
+              else{
+                user_Input +=event.target.textContent;
+              }
+            }
+          }
+        }
+      display_Inputs.textContent = user_Input; // display value on screen   
+      })
+    });  
+}
